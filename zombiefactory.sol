98938f304af3379e12bffd704c21ba4a39624b6b
sanghyeok <ksangh228@gmail.com>
2018-08-07 22:05:40 +0900

    zombie factory solidty code

--- zombiefactory.sol
+++ zombiefactory.sol
@@ -2,30 +2,36 @@ pragma solidity ^0.4.19;
 
 contract ZombieFactory {
 
-    event NewZombie(uint zombieId, string name, uint dna); // 사용자의 행동에 대한 피드백
+    event NewZombie(uint zombieId, string name, uint dna);
 
     uint dnaDigits = 16;
-    uint dnaModulus = 10 ** dnaDigits; // 10의 danDigits 승
+    uint dnaModulus = 10 ** dnaDigits;
 
-    struct Zombie { // struct 선언
+    struct Zombie {
         string name;
         uint dna;
     }
 
-    Zombie[] public zombies; // struct type dynamic size
+    Zombie[] public zombies;
 
-	// '_' is 관례상
+    mapping (uint => address) public zombieToOwner; // mapping key : uint, value: address
+    mapping (address => uint) ownerZombieCount; // default value is 0
+
     function _createZombie(string _name, uint _dna) private {
         uint id = zombies.push(Zombie(_name, _dna)) - 1;
+        zombieToOwner[id] = msg.sender; // msg.sender is transaction sender address
+        ownerZombieCount[msg.sender]++;
         NewZombie(id, _name, _dna);
-    } 
+    }
 
     function _generateRandomDna(string _str) private view returns (uint) {
+        require(onwerZombieCount[msg.sender] == 0); // true -> pass, false -> error 
         uint rand = uint(keccak256(_str));
         return rand % dnaModulus;
     }
 
     function createRandomZombie(string _name) public {
+        require(ownerZombieCount[msg.sender] == 0);
         uint randDna = _generateRandomDna(_name);
         _createZombie(_name, randDna);
     }
---
+++ zombiefeeding.sol
@@ -0,0 +1,7 @@
+pragma solidity ^0.4.19;
+
+import "./zombiefactory.sol"; // 상속을 위한 import
+
+contract ZombieFeeding is ZombieFactory {
+
+}
