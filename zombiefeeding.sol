81cb8935b596964844e891dc6d5e2f6bc96a24fa
sanghyeok <ksangh228@gmail.com>
2018-08-12 02:52:49 -0700

    get some data from other contract

--- zombiefeeding.sol
+++ zombiefeeding.sol
@@ -2,6 +2,41 @@ pragma solidity ^0.4.19;
 
 import "./zombiefactory.sol"; // 상속을 위한 import
 
+contract KittyInterface { // 외부 contract을 사용하기 위한 interface 
+  function getKitty(uint256 _id) external view returns (
+    bool isGestating,
+    bool isReady,
+    uint256 cooldownIndex,
+    uint256 nextActionAt,
+    uint256 siringWithId,
+    uint256 birthTime,
+    uint256 matronId,
+    uint256 sireId,
+    uint256 generation,
+    uint256 genes
+  );
+}
+
 contract ZombieFeeding is ZombieFactory {
+
+  address ckAddress = 0x06012c8cf97BEaD5deAe237070F9587f8E7A266d; // 외부 contract 주소
+  KittyInterface kittyContract = KittyInterface(ckAddress);
+
+  function feedAndMultiply(uint _zombieId, uint _targetDna, string _species) public {
+    require(msg.sender == zombieToOwner[_zombieId]);
+    Zombie storage myZombie = zombies[_zombieId];
+    _targetDna = _targetDna % dnaModulus;
+    uint newDna = (myZombie.dna + _targetDna) / 2;
+    if(keccak256(_species) == keccak256("kitty")){ // 문자열 비교를 위해 keccak256 사용
+      newDna = newDna - newDna % 100 + 99;
+    }
+    _createZombie("NoName", newDna);
+  }
+
+  function feedOnKitty(uint _zombieId, uint _kittyId) public {
+    uint kittyDna;
+    (,,,,,,,,,kittyDna) = kittyContract.getKitty(_kittyId);
+    feedAndMultiply(_zombieId, kittyDna, "kitty");
+  }
 
 }
