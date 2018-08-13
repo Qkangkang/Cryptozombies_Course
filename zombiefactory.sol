38a6d56cbe72fcd63ddb8cdec1781a3f0dac3901
sanghyeok <ksangh228@gmail.com>
2018-08-12 22:53:01 -0700

    Add count variables

--- zombiefactory.sol
+++ zombiefactory.sol
@@ -11,20 +11,22 @@ contract ZombieFactory is Ownable {
     uint cooldownTime = 1 days;
 
     struct Zombie {
-        string name;
-        uint dna;
-        uint32 level;
-        uint32 readyTime;
+      string name;
+      uint dna;
+      uint32 level;
+      uint32 readyTime;
+      uint16 winCount;
+      uint16 lossCount;
     }
 
     Zombie[] public zombies;
 
-    mapping (uint => address) public zombieToOwner; // mapping key : unint, value: address
-    mapping (address => uint) ownerZombieCount; // default value is 0
+    mapping (uint => address) public zombieToOwner;
+    mapping (address => uint) ownerZombieCount;
 
     function _createZombie(string _name, uint _dna) internal {
-
-        uint id = zombies.push(Zombie(_name, _dna,1,uint32(now + cooldownTime))) - 1;
+        
+        uint id = zombies.push(Zombie(_name, _dna, 1, uint32(now + cooldownTime), 0, 0)) - 1;
         zombieToOwner[id] = msg.sender;
         ownerZombieCount[msg.sender]++;
         NewZombie(id, _name, _dna);
